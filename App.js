02a171fb4ad74b7ef67cba3fba7c5fef10e64356
Michael Coustier <curiousmike@gmail.com>
2021-06-28 11:44:57 -0700

    add how to start in readme; add new googleMaps api key

--- App.js
+++ App.js
@@ -3,21 +3,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Footer from './footer.js';
 import Map from './map.js';
-import {Loader, LoaderOptions} from 'google-maps';
+import { Loader, LoaderOptions } from 'google-maps';
 // or const {Loader} = require('google-maps'); without typescript
- 
-const options: LoaderOptions = {/* todo */};
-const googleMapsAPIKey = 'AIzaSyAeawmHKQyyqWRxuHMbTXEkOIHR_MwgOKA';
+
+const options: LoaderOptions = {/* todo */ };
+// const googleMapsAPIKey = 'AIzaSyAeawmHKQyyqWRxuHMbTXEkOIHR_MwgOKA';
+const googleMapsAPIKey = 'AIzaSyBTWrjGNpeWPSKOyhdpeKNFoUohdPMzAvQ';
 const loader = new Loader(googleMapsAPIKey, options);
- 
-let  googleMaps = null;
+
+let googleMaps = null;
 const initGoogleMaps = async () => {
   if (googleMaps !== null) return;
   googleMaps = await loader.load();
   console.log('googleMaps = ', googleMaps);
   const map = new googleMaps.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8,
+    center: { lat: 37.75840718811915, lng: -122.44203189405526 },
+    zoom: 8,
   });
   console.log('map = ', map);
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 To start
-expo start
+1. Be sure latest version of npm is installed; npm install -g npm
+2. Install expo; npm install -g expo-cli
+3. npm install to install everything else
+
+
+expo start (or npm start or expo web)?
